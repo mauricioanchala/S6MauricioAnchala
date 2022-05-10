6899416279e3750786bbd5476448434b74a2046a
Mauricio Anchala Sanez <mauricio.anchala@gmail.com>
2022-05-09 21:51:06 -0500

    Agregar archivos de proyecto.

---
+++ S6MauricioAnchala/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net;
+
+namespace S6MauricioAnchala
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+                cliente.UploadValues("http://130.1.16.249/moviles/post.php", "Post", parametros);
+                await DisplayAlert("Alerta", "Dato Ingresado", "OK");
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Alerta", "Error: " + ex.Message, "ok");
+            }
+        }
+
+        private void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ S6MauricioAnchala/Page1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+namespace S6MauricioAnchala
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        private const string Url = "http://130.1.16.249/moviles/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<S6MauricioAnchala.Datos> _post;
+
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnConsultar_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<S6MauricioAnchala.Datos> posts = JsonConvert.DeserializeObject<List<S6MauricioAnchala.Datos>>(content);
+            _post = new ObservableCollection<S6MauricioAnchala.Datos>(posts);
+            MyListView.ItemsSource = _post;
+        }
+    }
+}
